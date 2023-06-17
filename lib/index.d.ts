b40bed12e9f46adab3c8100c81b8077e8f459956
GoodBoyNeon <goodboyneon03@gmail.com>
2023-06-17 13:25:08 +0545

    fix: add lib/ to the repo

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+declare const logger: {
+    error: (message: string) => void;
+    info: (message: string) => void;
+    success: (message: string) => void;
+    warn: (message: string) => void;
+};
+
+export { logger };
