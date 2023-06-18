71ce08550a4f683aac53b2567980db1d2d962225
GoodBoyNeon <goodboyneon03@gmail.com>
2023-06-18 19:48:29 +0545

    fix: remove ./lib/ from the repository
    ./lib/ is no longer needed in the repository. The folder can be obtained my compiling when needed

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const logger: {
-    error: (message: string) => void;
-    info: (message: string) => void;
-    success: (message: string) => void;
-    warn: (message: string) => void;
-};
-
-export { logger };
